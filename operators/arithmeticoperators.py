a2837df51377b53b8b29a6720eb1e7c74b809d6e
jeevitha98 <jeevithahj3@gmail.com>
2023-03-28 00:07:08 +0530

    operators

---
+++ operators/arithmeticoperators.py
@@ -0,0 +1,8 @@
+a,b=2,6
+print('add',a+b)
+print('sub',a-b)
+print('mul',a*b)
+print('div',a/b)
+print('mod',a%b)
+print('pow',a**b)
+print('floor',a//b)
---
+++ operators/operators.py
@@ -0,0 +1,10 @@
+#body mass index
+
+height=5.2
+weight=58
+
+#bmi=weight(kg)/height(m)
+
+heightinmeter=height*0.4536
+bmi=weight/heightinmeter
+print('bmi',bmi)
