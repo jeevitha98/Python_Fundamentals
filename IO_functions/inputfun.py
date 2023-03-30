f56792e2d7afe2f062b8bd4ac434159339fbc6cc
jeevitha98 <jeevithahj3@gmail.com>
2023-03-30 23:57:02 +0530

    input function

--- IO_functions/inputfun.py
+++ IO_functions/inputfun.py
@@ -1,4 +1,8 @@
-s=input() #to take input from user
+'''s=input() #to take input from user
 print(s)
 
 s=input('enter your name')
+print(s)
+'''
+i=int(input('enter a integer num')) #to take only integer input-typecasting
+print(i)
