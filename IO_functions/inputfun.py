20d60e1f0b4535d1fc7a7aba9ced56dcaddc48a8
jeevitha98 <jeevithahj3@gmail.com>
2023-03-30 23:57:51 +0530

    input function

--- IO_functions/inputfun.py
+++ IO_functions/inputfun.py
@@ -6,3 +6,6 @@ print(s)
 '''
 i=int(input('enter a integer num')) #to take only integer input-typecasting
 print(i)
+
+lst=[x for x in input('enter 3 numbers').split()] #seperated by space
+print(lst)
