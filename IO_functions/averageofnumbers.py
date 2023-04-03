195422f2ad4d7bad1121806e4cac2f223fb77a3f
jeevitha98 <jeevithahj3@gmail.com>
2023-04-03 23:59:21 +0530

    calculate average of three numbers

--- IO_functions/averageofnumbers.py
+++ IO_functions/averageofnumbers.py
@@ -1,4 +1,6 @@
 #calculate average of three numbers
 a,b,c=[int(x) for x in input('enter 3 int numbers').split()]
+d,e,c=[int(y) for y in input('enter 3 int numbers').split()]
+
 avg=(a+b+c)/3
 print('average of 3 numbers is',avg)
