724728b777a1c5dbba394699592439f888769009
jeevitha98 <jeevithahj3@gmail.com>
2023-04-03 23:50:40 +0530

    read and dsiplay student details

--- IO_functions/studentdetails.py
+++ IO_functions/studentdetails.py
@@ -1,6 +1,6 @@
 studid=int(input('enter student id'))
 name=str(input('enter student name'))
-marks=floatinput(('enter marsks'))
+marks=float(input(('enter marsks'))
 
 print('id:',studid, 'name:',name, 'marks:',marks)
 print('id:%d',studid, 'name:%s',name, 'marks:%f',marks)
