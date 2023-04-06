fcc8120c6e4d191838d0c927937c0189af7c31c7
jeevitha98 <jeevithahj3@gmail.com>
2023-04-07 00:53:45 +0530

    program to count vowels in a word

---
+++ controlstatements/vowelcounter.py
@@ -0,0 +1,9 @@
+word=input('enter a word')
+vowels={'a','e','i','o','u'}
+results={}
+for c in word:
+    if c in vowels:
+        results[c]=results.get(c, 0)+1
+
+for k, v in results.items():
+    print(k,"is present",v,"times")
