b72e841e21cc8f9d997f8dbd8155962430ea4a8e
jeevitha98 <jeevithahj3@gmail.com>
2023-04-07 01:17:09 +0530

    program to print employee info

---
+++ controlstatements/employeeinfo.py
@@ -0,0 +1,7 @@
+n=int(input('enter the number of employees'))
+employees={}
+for i in range(n):
+    name=input('enter employee name')
+    salary=input('enter employee salary')
+    employees[name]=salary
+
