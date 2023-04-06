d0f85cdf61e83cf366d253e41c7046e91e18bc12
jeevitha98 <jeevithahj3@gmail.com>
2023-04-06 23:58:28 +0530

    remove dup entries

--- controlstatements/removedup.py
+++ controlstatements/removedup.py
@@ -1,2 +1,8 @@
+#remove dup entries in a list
 l1=eval(input('enter a list of elements'))
 print(l1)
+l2=[]
+for each_value in l1:
+    if each_value not in l2:
+        l2.append(each_value)
+print(l2)
