bdc19afb315fb6ce24bf9f048dd7a59fade9c3a2
jeevitha98 <jeevithahj3@gmail.com>
2023-04-09 01:06:23 +0530

    reversing a string

---
+++ controlstatements/stringreversal.py
@@ -0,0 +1,15 @@
+'''s=input('enter a string')
+print(s[::-1])'''
+
+s1=input('enter a string')
+i=len(s1)-1
+result=''
+
+while i >= 0:
+    result=result+s1[i]
+    i=i-1
+
+print(result)
+
+s2='-'.join(['a','b','c'])
+print(s2)
