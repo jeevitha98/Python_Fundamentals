9accaf89957afad22610bf883f11cb38608b1dce
jeevitha98 <jeevithahj3@gmail.com>
2023-04-09 01:23:23 +0530

    program to reverse chars of a string

---
+++ controlstatements/reversechar.py
@@ -0,0 +1,17 @@
+'''Super Cool Python
+repuS looC nohtyP'''
+
+s='python is cool'
+temp=s.split()
+print(temp)
+result=[]
+i=0
+
+while i < len(temp):
+    result.append(temp[i][::-1])
+    i=i+1
+
+print(result)
+
+output=' '.join(result)
+print(output)
