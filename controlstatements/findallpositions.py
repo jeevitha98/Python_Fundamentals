ac856278d67235e02085e5681bed15bca30c6299
jeevitha98 <jeevithahj3@gmail.com>
2023-04-10 01:25:16 +0530

    prog to print positions of a substring in a string

---
+++ controlstatements/findallpositions.py
@@ -0,0 +1,16 @@
+'''prog to print psoitions of a substring in a string'''
+s="take up one idea and make that idea your life."\
+  "think and dream of that idea ad leave every other idea alone."
+
+subs="idea"
+fount= False
+pos= -1
+length=len(s)
+while True:
+    pos = s.find(subs,pos+1,length)
+    if pos == -1:
+        break
+    print("found the substring at position", pos)
+    found=True
+if found==False:
+    print("sub string not found")
