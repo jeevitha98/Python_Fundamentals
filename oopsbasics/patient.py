d455203b9bb0bf30f986d6f2bbd4f40e777ca7eb
jeevitha98 <jeevithahj3@gmail.com>
2023-04-23 00:13:38 +0530

    patient details

---
+++ oopsbasics/patient.py
@@ -0,0 +1,25 @@
+class Patient:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def addClinicalData(self,clinical):
+        self.clinical.append(clinical)
+
+class Clinical:
+    def __init__(self,componentName,componentValue) :
+        self.componentname=componentName
+        self.componentvalue=componentValue
+
+p = Patient('john',40)
+
+c1=Clinical('bp','88/122')
+p.addClinicalData(c1)
+
+c2=Clinical('bp','100/122')
+p.addClinicalData(c2)
+
+print(p.name)
+for eachClinical in p.clinical:
+    print(eachClinical.componentName)
+    print(eachClinical.componentValee)
