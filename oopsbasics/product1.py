36a238f04ba73a16ef1e7a7b7d0d1df497bb7ec6
jeevitha98 <jeevithahj3@gmail.com>
2023-04-22 23:24:24 +0530

    product - class and its objects

---
+++ oopsbasics/product1.py
@@ -0,0 +1,22 @@
+class Product:
+    def __init__(self) :
+        self.name="iphone"
+        self.description="excellent"
+        self.price=700
+
+    def display(self):
+        print(self.name)
+        print(self.description)        
+        print(self.price)        
+
+
+
+p1=Product()
+p1.display()
+
+p2=Product()
+p2.display()
+
+
+
+        
