c68dae0b8543de71025c8bef443dd11e8db765a7
jeevitha98 <jeevithahj3@gmail.com>
2023-04-25 23:51:55 +0530

    bmw

---
+++ inheritance/bmw.py
@@ -0,0 +1,11 @@
+class BMW:
+    def __init__(self,make,model,year):
+        self.make=make
+        self.model=model
+        self.year=year
+
+class ThreeSeries(BMW):
+    def __init__(self, cruiseControlEnabled, make, model, year):
+        BMW.__init__(self,make, model, year)
+        self.cruiseControlEnabled = cruiseControlEnabled
+
