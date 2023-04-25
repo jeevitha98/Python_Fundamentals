713db78e05d712b7dfe30028fdeec838e2268436
jeevitha98 <jeevithahj3@gmail.com>
2023-04-25 23:52:18 +0530

    bmw

--- inheritance/bmw.py
+++ inheritance/bmw.py
@@ -9,3 +9,9 @@ class ThreeSeries(BMW):
         BMW.__init__(self,make, model, year)
         self.cruiseControlEnabled = cruiseControlEnabled
 
+
+class FiveSeries(BMW):
+    def __init__(self, parkingAssistEnabled, make, model, year):
+        BMW.__init__(self,make, model, year)
+        self.parkingAssistEnabled = parkingAssistEnabled
+        
