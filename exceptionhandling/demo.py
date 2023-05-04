7d6944dd1a50886b3648b12e207fa385491c7931
jeevitha98 <jeevithahj3@gmail.com>
2023-05-04 23:55:55 +0530

    demo

---
+++ exceptionhandling/demo.py
@@ -0,0 +1,13 @@
+#division by zero exception
+try:
+    a,b=[int(x) for x in input("enter two numbers").split()]
+    c=a/b
+    print(c)
+
+except ZeroDivisionError:
+    print("division by zero not allowed")
+    print("please enter a non zero number")
+
+print("code after exception")
+
+
