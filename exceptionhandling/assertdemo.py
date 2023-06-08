07323c426afe63e4460b61478cefda653bca0d69
jeevitha98 <jeevithahj3@gmail.com>
2023-06-08 23:32:22 +0530

    assertion with try-catch

--- exceptionhandling/assertdemo.py
+++ exceptionhandling/assertdemo.py
@@ -1,2 +1,11 @@
-num=int(input("enter an even num"))
-assert num%2==0, "input invalid or odd num"
+#num=int(input("enter an even num"))
+#assert num%2==0, "input invalid or odd num"
+
+try:
+    num=int(input("enter an even num"))
+    assert num%2==0, "input invalid or odd num"
+
+except AssertionError as obj:
+    print(obj)
+
+    print("after assertion")
